8b4013295892936f937f79d3b959f2241cd0d55e
nglushien17 <noahcg@gmail.com>
2019-10-31 20:14:52 -0400

    chapter navbar component

---
+++ src/components/ChapterNavbar.vue
@@ -0,0 +1,96 @@
+<template>
+  <section>
+    <b-navbar class="chapters" toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand>Chapters</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">I. There Was A Time</b-nav-item>
+          <b-nav-item href="#">II. Slow Descent</b-nav-item>
+          <b-nav-item href="#">III. Let Us Start</b-nav-item>
+          <b-nav-item href="#">IV. Exhiled to Nowhere</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "chapternavbar"
+};
+</script>
+<style lang="scss" scoped>
+.chapters {
+  background: #111 !important;
+}
+.navbar-brand {
+  display: inline-block;
+}
+.navbar-nav {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  list-style: none;
+  display: block;
+  width: 100%;
+  text-align: center;
+  background-color: #111;
+}
+.nav-item {
+  display: block;
+}
+.navbar-dark .navbar-nav .nav-link {
+  display: block;
+  padding: 1em;
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.15rem;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+.navbar-dark .navbar-nav .nav-link:hover {
+  color: #bc9728;
+  text-decoration: underline;
+}
+.navbar-dark .navbar-nav .nav-link:active,
+.navbar-dark .navbar-nav .nav-link:focus {
+  border: 0;
+  color: #bc9728;
+  outline: none;
+  text-decoration: underline;
+}
+@media (min-width: 992px) {
+  .chapters {
+    padding: 0;
+  }
+  .navbar-brand {
+    display: none;
+  }
+  .navbar-nav {
+    display: table;
+    table-layout: fixed;
+  }
+  .nav-item {
+    display: table-cell;
+  }
+  .navbar-dark .navbar-nav .nav-link {
+    font-size: 0.9rem;
+  }
+  .navbar-dark .navbar-nav .nav-link:hover {
+    border-bottom: 3px solid #bc9728;
+    color: #bc9728;
+    text-decoration: none;
+  }
+  .navbar-dark .navbar-nav .nav-link:active,
+  .navbar-dark .navbar-nav .nav-link:focus {
+    border-bottom: 3px solid #bc9728;
+    text-decoration: none;
+  }
+}
+@media (min-width: 1200px) {
+  .navbar-dark .navbar-nav .nav-link {
+    font-size: 1.15rem;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,57 +3,11 @@
     <section class="panel home-panel">
       <b-container>
         <b-row>
-          <b-col class="column-left">
-            <a href="#">Link 1</a>
-            <img src="https://picsum.photos/id/278/500/500" alt />
-          </b-col>
-          <b-col class="column-right">
-            <a href="#">Link 2</a>
-            <p
-              class="text-left"
-            >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nisi alias enim consequuntur. Debitis consequuntur minus fugiat nam provident assumenda ipsum eligendi quam! Odio nesciunt, iste quas reprehenderit fugit rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis odio molestiae beatae, atque provident maiores quam ad, non libero est dicta iusto incidunt maxime, numquam exercitationem quae ab dolore.</p>
-            <p
-              class="text-left"
-            >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nisi alias enim consequuntur. Debitis consequuntur minus fugiat nam provident assumenda ipsum eligendi quam! Odio nesciunt, iste quas reprehenderit fugit rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corporis odio molestiae beatae, atque provident maiores quam ad, non libero est dicta iusto incidunt maxime, numquam exercitationem quae ab dolore.</p>
+          <b-col class="py-4">
+            <h1 class="text-left text-white">Chapter Navbar</h1>
           </b-col>
         </b-row>
-      </b-container>
-      <flyout button-image="https://picsum.photos/id/386/100/100">
-        <template v-slot:heading>
-          <p class="title text-left">{{ flyout.title }}</p>
-          <p class="sub-title text-left">{{ flyout.subtitle }}</p>
-        </template>
-        <template v-slot:media>
-          <audio-player
-            source="https://encyclopedia.ushmm.org/asset/7a639b01-b5dc-4506-bd9c-b56ad6fba978.mp3"
-            captions="/media/lyrics.vtt"
-          />
-        </template>
-      </flyout>
-    </section>
-    <section>
-      <b-container>
-        <b-row>
-          <h2>second</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <a href="#">Link 3</a>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-        </b-row>
-      </b-container>
-    </section>
-    <section>
-      <b-container>
-        <b-row>
-          <h2>third</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa error, impedit accusamus, velit similique perferendis saepe architecto cupiditate voluptates eum enim eaque cum magnam veritatis illum molestias nostrum quasi.</p>
-        </b-row>
+        <chapter-navbar />
       </b-container>
     </section>
   </div>
@@ -61,22 +15,12 @@
 
 <script>
 // @ is an alias to /src
-import Flyout from "@/components/Flyout.vue";
-import AudioPlayer from "@/components/AudioPlayer.vue";
+import ChapterNavbar from "@/components/ChapterNavbar.vue";
 
 export default {
   name: "home",
   components: {
-    Flyout,
-    AudioPlayer
-  },
-  data() {
-    return {
-      flyout: {
-        title: "Mohammed",
-        subtitle: "The Mullah/Father/Husband"
-      }
-    };
+    ChapterNavbar
   }
 };
 </script>
