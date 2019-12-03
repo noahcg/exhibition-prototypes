3cd48bed187afeb48a138ae0ce463bb4e1e3a075
nglushien17 <noahcg@gmail.com>
2019-12-03 10:28:53 -0500

    started working on a prototype for the story page

---
+++ src/views/Story.vue
@@ -0,0 +1,337 @@
+<template>
+  <div>
+    <section class="story-intro">
+      <figure class="story-intro-background">
+        <img src="images/hero-bg.jpg" alt="A Rohingya Village" />
+      </figure>
+      <b-container>
+        <b-row>
+          <b-col>
+            <b-row>
+              <b-col md="10" offset-md="1" xl="8" offset-xl="2">
+                <figure class="story-featured-image">
+                  <div class="mobile-screen"></div>
+                  <img src="images/Jomila.jpg" alt="Portrait of Jomila, the Mother/Wife" />
+                </figure>
+                <div class="intro-text">
+                  <p class="page-title">{{ title }}</p>
+                  <div class="name-group">
+                    <p class="name">{{ name }}</p>
+                    <p class="subtitle">{{ subtitle }}</p>
+                  </div>
+                </div>
+              </b-col>
+            </b-row>
+            <div
+              class="intro-snippet"
+            >"We thought that the military would just come to tkae his books away, but it turned out that they murdered him instead."</div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <section class="content">
+      <div class="container">
+        <b-row>
+          <b-col lg="10" offset-lg="1">
+            <b-row>
+              <b-col cols="12" md="5" class="content-left">
+                <figure>
+                  <img src="images/shirt.png" alt="Image of a shirt being shown by Jomila" />
+                  <figcaption>This is an image caption.</figcaption>
+                </figure>
+              </b-col>
+              <b-col cols="12" md="7" class="content-right">
+                <h1 class="section-title">The Record Keeper</h1>
+                <h2>Part 1</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </div>
+    </section>
+    <section class="content">
+      <div class="container">
+        <b-row>
+          <b-col lg="10" offset-lg="1">
+            <b-row>
+              <b-col cols="12" md="5" class="content-left">
+                <figure>
+                  <img src="images/shirt.png" alt="Image of a shirt being shown by Jomila" />
+                  <figcaption>This is an image caption.</figcaption>
+                </figure>
+              </b-col>
+              <b-col cols="12" md="7" class="content-right">
+                <h2>Part 2</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </div>
+    </section>
+    <section class="content">
+      <div class="container">
+        <b-row>
+          <b-col lg="10" offset-lg="1">
+            <b-row>
+              <b-col cols="12" md="5" class="content-left">
+                <figure>
+                  <!-- <img src="images/shirt.png" alt="Image of a shirt being shown by Jomila" /> -->
+                  <!-- <figcaption>This is an image caption.</figcaption> -->
+                </figure>
+              </b-col>
+              <b-col cols="12" md="7" class="content-right">
+                <h2>Part 3</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </div>
+    </section>
+    <section class="content">
+      <div class="container">
+        <figure class="fullscreen-section-image">
+          <img src="images/section-bkg.jpg" alt="A group of Rohingya citizens wait in a line." />
+        </figure>
+        <b-row>
+          <b-col lg="10" offset-lg="1">
+            <b-row>
+              <b-col cols="12" md="5" class="content-left">
+                <figure>
+                  <img src="images/shirt.png" alt="Image of a shirt being shown by Jomila" />
+                  <figcaption>This is an image caption.</figcaption>
+                </figure>
+              </b-col>
+              <b-col cols="12" md="7" class="content-right">
+                <h2>Part 4</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </div>
+    </section>
+    <section class="content">
+      <div class="container">
+        <figure class="fullscreen-section-image">
+          <!-- <img src="images/section-bkg.jpg" alt="A group of Rohingya citizens wait in a line." /> -->
+        </figure>
+        <b-row>
+          <b-col lg="10" offset-lg="1">
+            <b-row>
+              <b-col cols="12" md="5" class="content-left">
+                <figure>
+                  <img src="images/shirt.png" alt="Image of a shirt being shown by Jomila" />
+                  <figcaption>This is an image caption.</figcaption>
+                </figure>
+              </b-col>
+              <b-col cols="12" md="7" class="content-right">
+                <h2>Part 5</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quaerat eius aperiam deserunt quisquam vitae sunt magni? Ex voluptates rem qui exercitationem aperiam similique voluptatibus explicabo. Quasi, recusandae. Tempore, id?</p>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  data() {
+    return {
+      title: "The Record Keeper",
+      name: "Jomila",
+      subtitle: "The Mother/Wife"
+    };
+  }
+};
+</script>
+<style lang="scss">
+body {
+  background-color: #282828 !important;
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+.story-intro-background {
+  display: none;
+}
+.story-featured-image {
+  left: 0;
+  position: fixed;
+  top: 0;
+
+  .mobile-screen {
+    background: rgba(0, 0, 0, 0.65);
+    display: block;
+    height: 100vh;
+    position: absolute;
+    width: 100%;
+    z-index: 1;
+  }
+
+  img {
+    margin-left: -25%;
+    width: 100vh;
+  }
+}
+.intro-text {
+  color: #fff;
+  padding: 40px 0 0;
+  position: relative;
+  text-align: left;
+}
+
+.name-group {
+  border-left: 5px solid #bd9727;
+  padding-left: 15px;
+}
+
+.page-title,
+.subtitle {
+  text-transform: uppercase;
+}
+
+.page-title {
+  font-size: 3.6rem;
+  line-height: 1;
+  margin-bottom: 10px;
+}
+
+.name {
+  font-size: 1.6rem;
+  line-height: 1;
+  margin-bottom: 10px;
+}
+.subtitle {
+  font-size: 0.8rem;
+  line-height: 1;
+}
+
+.intro-snippet {
+  color: white;
+  text-align: left;
+  font-size: 1.2rem;
+  position: relative;
+}
+.content {
+  color: #fff;
+  position: relative;
+  text-align: left;
+}
+.fullscreen-section-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.fullscreen-section-image img {
+  height: 100%;
+  left: 0;
+  min-width: 100vw;
+  object-fit: cover;
+  top: 0;
+  width: 100%;
+}
+.content-left {
+  padding-top: 100px;
+  text-align: right;
+}
+.content-left figure {
+  img {
+    width: 100%;
+  }
+  figcaption {
+    margin-right: 15px;
+  }
+}
+
+@media (min-width: 768px) {
+  .story-intro {
+    margin-bottom: 100px;
+    padding: 40px 0 0;
+  }
+  .story-intro-background {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .story-intro-background img {
+    height: 100%;
+    left: 0;
+    min-height: 100vh;
+    min-width: 100vw;
+    -o-object-fit: cover;
+    object-fit: cover;
+    // position: absolute;
+    top: 0;
+    width: 100%;
+  }
+  .story-featured-image {
+    float: left;
+    max-width: 280px;
+    position: relative;
+
+    .mobile-screen {
+      display: none;
+    }
+    img {
+      margin: 0;
+      width: 100%;
+    }
+  }
+  .intro-text {
+    float: left;
+    margin: 20px 0 0 -25px;
+    width: 40%;
+  }
+  .intro-snippet {
+    width: 360px;
+    float: right;
+    margin-top: -110px;
+    margin-right: 20px;
+    font-size: 1.2rem;
+  }
+
+  .content {
+    background: none;
+    margin-bottom: 50px;
+  }
+}
+@media (min-width: 992px) {
+  .story-intro {
+    margin-bottom: 200px;
+  }
+  .story-featured-image {
+    max-width: 320px;
+  }
+  .intro-snippet {
+    width: 400px;
+    margin-top: -140px;
+    margin-right: 140px;
+  }
+}
+</style>
