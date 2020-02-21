6e8d9ebfbcfa9688d08c79db5fb2765a88b1ee39
nglushien17 <noahcg@gmail.com>
2020-02-20 21:01:14 -0500

    added video panel

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Panel1 from "../views/Panel1.vue";
 import Panel2Image from "../views/Panel2-Image.vue";
+import Panel2Video from "../views/Panel2-Video.vue";
 import Panel3 from "../views/Panel3.vue";
 import Panel5 from "../views/Panel5.vue";
 
@@ -17,6 +18,11 @@ const routes = [
     path: "/panel2-image",
     name: "panel2-image",
     component: Panel2Image
+  },
+  {
+    path: "/panel2-video",
+    name: "panel2-video",
+    component: Panel2Video
   },
   {
     path: "/panel3",
